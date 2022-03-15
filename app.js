6943ccc6d2970650297c656e92e50ccc99e7d560
stack0801 <stack0801@gmail.com>
2022-03-15 16:58:35 +0900

    역할 분담

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+//static 으로
+app.use(express.static('Front/'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/Front/html/index.html')
+})
+
+app.listen(3000)
