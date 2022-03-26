ab3c986ff7d44a130b94928e20c0d9146703d4e8
stack0801 <stack0801@gmail.com>
2022-03-26 19:30:36 +0900

    sign in, up 햇갈려서 나온 오타, 변수 수정

--- Front/js/sign_in.js
+++ Front/js/sign_in.js
@@ -1,3 +1,30 @@
 window.onload = () => {
 	console.log("!!!")
 }
+
+const sign_up_btn = document.getElementById("sign_in-button")
+const id_ipt = document.getElementById("id_ipt")
+const pw_ipt = document.getElementById("pw_ipt")
+
+
+sign_up_btn.onclick = () => {
+	const id = id_ipt.value
+	const pw = pw_ipt.value
+
+	//axios 비동기 통신
+	axios({
+		method: 'post',
+		url: '/sign_up',
+		data: {
+		  	id: id,
+		  	pw: pw
+		}
+	})
+	.then((res) => {
+		alert(res.data)
+
+		if(res.data == 'success') {
+			window.location.href='/'
+		}
+	})
+}
