4b5a18454278cb07ca005768e7376cd6826aa42d
stack0801 <stack0801@gmail.com>
2022-04-18 15:22:05 +0900

    이미지 업로드 기능 구현

--- Front/js/boardtest.js
+++ Front/js/boardtest.js
@@ -3,22 +3,24 @@ window.onload = () => {
 }
 
 const board_in = document.getElementById("board_in")
+const board_img = document.getElementById("upload_icon")
+const messageElement = document.getElementById("message")
 
 board_in.onclick = () => {
-    const messageElement = document.getElementById("message")
-    const message = messageElement.value
+
+    const formData = new FormData()
+    formData.append("img", board_img.files[0])
+    formData.append("message", messageElement.value)
 
     axios({
         method: 'post',
         url: '/boardtest',
-        data: {
-            message: message
-        }
+        header: { 'content-type': 'multipart/form-data' },
+        data: formData
     })
-        .then((res) => {
-            alert(res.data)
-
-			if (res.data == 'success') 
-				window.location.href = '/'
-        })
+    .then((res) => {
+        alert(res.data)
+        if (res.data == 'success') 
+            window.location.href = '/'
+    })
 }
