4e7f65d9b92e5baa3fc27ee8928ce07f3fa43af1
stack0801 <stack0801@gmail.com>
2022-05-02 04:17:07 +0900

    수습

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
-import './Landing.css'
-import { FaSearch } from 'react-icons/fa';
-import { FaArrowUp } from 'react-icons/fa';
-import { FaUser } from 'react-icons/fa';
+import { FaArrowUp, FaSearch, FaUser } from 'react-icons/fa';
 import axios from 'axios'
+import './LandingPage.css'
 import logo from './logo.svg'
 import top_image from "./watercolor.jpg"
 
@@ -58,81 +56,6 @@ export default function Main() {
         });
     };
 
-    
-    return (
-        <>
-            {scrollPosition < 900 ? (
-                <div className="navbar">
-                    <div className="navbar_logo">
-                        <a href="/">Home</a>
-                    </div>
-
-                    <ul className="navbar_menu">
-                        <li className="navbar_menu_menu">
-                            <a href="#!">menu1</a>
-                        </li>
-                        <li className="navbar_menu_menu">
-                            <a href="#!">menu2</a>
-                        </li>
-                        <li className="navbar_menu_menu">
-                            <a href="#!">menu3</a>
-                        </li>
-                    </ul>
-                    
-                    <a href="/sign_in"><FaUser className="user_icon" size="26" color="white" /></a>
-                    {/*<ul className="navbar_icons">
-                        {sign ? (
-                            <li><Link to="/sign_in"><a onClick={onClick}>로그인</a></Link></li>
-                        ) : (
-                            <li><Link to="/sign_up"><a onClick={onClick}>회원가입</a></Link></li>
-                        )}
-                        </ul>*/}
-                </div>
-            ) : (
-                <div className="navbar">
-                    <div className="navbar_logo">
-                        <a href="/">Home</a>
-                    </div>
-
-                    <div id="search_box">
-                        <input id="search_ipt" placeholder="keyword" type="text"></input>
-                        <button><FaSearch className="search_icon" size="26" color="white" /></button>
-                    </div>
-
-                    <a href="/sign_in"><FaUser className="user_icon" size="26" color="white" /></a>
-                    {/*<ul className="navbar_icons">
-                        {sign ? (
-                            <li><Link to="/sign_in"><a onClick={onClick}>로그인</a></Link></li>
-                        ) : (
-                            <li><Link to="/sign_up"><a onClick={onClick}>회원가입</a></Link></li>
-                        )}
-                        </ul>*/}
-                    </div>
-            )}
-
-            <div className="outer">
-                <div id="top">
-                    <div></div>
-                    <h1>GET FREE ICONS</h1>
-                    <div id="search_box">
-                        <input id="search_ipt" placeholder="keyword" type="text"></input>
-                        <button><FaSearch className="search_icon" size="26" color="white" /></button>
-                    </div>
-                </div>
-
-                <div className="inner bg-yellow">1</div>
-                <div className="divider"></div>
-                <div className="inner bg-blue">2</div>
-                <div className="divider"></div>
-                <div className="inner bg-pink">3</div>
-
-            </div>
-
-            {ShowBtn && (
-                <button onClick={scrollTop}><FaArrowUp className="back_top" size="26" color="white" />
-                </button>
-            )}
-        </>
     //search_box 변수화
     const search_box = (
         <div id = "search_box">
@@ -147,8 +70,9 @@ export default function Main() {
             {scrollPosition < 500 ? 
             <Link to = "/"><div>menu</div></Link>:
             search_box}
+            <FaUser className="user_icon" size="26" color="white" />
             {sign === null ?
-            <Link to = "/sign_in"><div>sign in(null)</div></Link> :
+            <Link to = "/sign_in"><div>sign in (null)</div></Link> :
             <Link to = "/sign_up"><div>sign up</div></Link>}
         </div>
         <div id="top">
