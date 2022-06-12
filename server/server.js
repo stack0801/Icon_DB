39e2e78f4078064de76709d501fec644323f9643
MercuriKim <xsssxtntjd@naver.com>
2022-06-12 18:15:37 +0900

    server.js 해시태그 검색 app.post('/tag') 추가

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.static('icons/'))                       //폴더를 클라이언
 app.use(express.static('Front/'))                       //react 를 사용함으로서 추후에 삭제 가능
 
 app.use(express.json())                                 //json 방식으로 통신
-app.use(express.urlencoded({extended:false}))           //post 로 받은 값에서 req.body를 읽을 수 있게함 //근데 솔직히 뭔뜻인지 모르겠음
+app.use(express.urlencoded({ extended: false }))           //post 로 받은 값에서 req.body를 읽을 수 있게함 //근데 솔직히 뭔뜻인지 모르겠음
 app.use(session_stream)
 
 //html
@@ -26,7 +26,7 @@ app.get('/sign_in', (req, res) => {
 })
 
 app.get('/boardtest', (req, res) => {
-    if(req.session.sign)
+    if (req.session.sign)
         res.sendFile(__dirname + '/Front/html/boardtest.html')
     else
         res.redirect('/')
@@ -44,7 +44,7 @@ app.post('/sign_up', (req, res) => {
         sql_pool.query(sql, [id, password, name], (err, result) => {
             if (err)
                 res.send("fail")
-            else          
+            else
                 res.send("success")
         })
     }
@@ -65,7 +65,7 @@ app.post('/sign_in', (req, res) => {
                 req.session.sign = id
                 res.send("success")
             }
-            else          
+            else
                 res.send("fail")
         })
     }
@@ -74,7 +74,7 @@ app.post('/sign_in', (req, res) => {
 })
 
 app.post('/get_auth', (req, res) => {
-    if (req.session.sign) 
+    if (req.session.sign)
         res.send(req.session.sign)
     else
         res.send("null")
@@ -125,32 +125,32 @@ const multer = require('multer');
 const multerS3 = require('multer-s3-transform');
 
 AWS.config.update({
-    region : 'us-east-1',
-    accessKeyId : process.env.AWS_ACCESSKEY,
-    secretAccessKey : process.env.AWS_SECRETACCESSKEY
+    region: 'us-east-1',
+    accessKeyId: process.env.AWS_ACCESSKEY,
+    secretAccessKey: process.env.AWS_SECRETACCESSKEY
 })
 
 var s3 = new AWS.S3()
- 
+
 var upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'webservicegraduationproject',
-    acl: 'public-read-write',
-    key: function(req, file, cb) {
-        const sql = 'SELECT content_id FROM content ORDER BY content_id DESC LIMIT 1'
-        sql_pool.query(sql, (err, rows, result) => {
-          if (err)
-            console.log(err)
-          else{
-            const filedirectory = 'img/' + (rows[0].content_id + 1)+ '.' + file.originalname.split('.').pop()
-            console.log(filedirectory)
-            cb(null, filedirectory);
-          }     
-        })
-    },
-    limits: { fileSize: 1000 * 1000 * 10 }
-  })
+    storage: multerS3({
+        s3: s3,
+        bucket: 'webservicegraduationproject',
+        acl: 'public-read-write',
+        key: function (req, file, cb) {
+            const sql = 'SELECT content_id FROM content ORDER BY content_id DESC LIMIT 1'
+            sql_pool.query(sql, (err, rows, result) => {
+                if (err)
+                    console.log(err)
+                else {
+                    const filedirectory = 'img/' + (rows[0].content_id + 1) + '.' + file.originalname.split('.').pop()
+                    console.log(filedirectory)
+                    cb(null, filedirectory);
+                }
+            })
+        },
+        limits: { fileSize: 1000 * 1000 * 10 }
+    })
 })
 
 // const s3 = new AWS.S3({
@@ -193,7 +193,7 @@ var upload = multer({
 app.post('/boardtest', upload.single('img'), (req, res) => {
 
     console.log(req.body)
-    
+
     const id = req.session.sign
     const message = req.body.message
 
@@ -205,13 +205,42 @@ app.post('/boardtest', upload.single('img'), (req, res) => {
     res.send("success")
 })
 
-app.post('/search', (req, res) => { 
+app.post('/search', (req, res) => {
     const sql = 'SELECT * FROM content where message LIKE' + " '%" + req.body.searchbox + "%' "
     sql_pool.query(sql, (err, result) => {
         if (err)
             throw err
         else
             res.send(result)
+    })
+})
+
+app.post('/tag', (req, res) => {
+    const search_tag = req.body.search_tag
+    const sql_Hashtag = 'SELECT Hash_id FROM Hash WHERE Hashtag = ?'
+    sql_pool.query(sql_Hashtag, [search_tag], (err_tag, rows_tag, result_tag) => {
+        if (err_tag)
+            console.log(err_tag)
+        else {
+            const Hash_id = rows_tag[0].Hash_id
+            const sql_content_idx = 'SELECT content_idx FROM content_has_hash WHERE Hash_idx = ?'
+            sql_pool.query(sql_content_idx, [Hash_id], (err_idx, rows_idx, result_idx) => {
+                if (err_idx)
+                    console.log(err_idx)
+                else {
+                    for (var i = 0; i < rows_idx.length; i++) {
+                        const sql_message = 'SELECT message FROM content WHERE content_id = ?'
+                        sql_pool.query(sql_message, [rows_idx[i].content_idx], (err_content, result_content) => {
+                            if (err_content)
+                                console.log(err_content)
+                            else {
+                                console.log(result_content)
+                            }
+                        })
+                    }
+                }
+            })
+        }
     })
 })
 
@@ -244,7 +273,7 @@ app.post('/content_update', (req, res) => {
             res.send("success")
     })
 
-    if(req.body.image != null){
+    if (req.body.image != null) {
         s3.deleteObject({
             Bucket: 'webservicegraduationproject',
             Key: 'img/' + content_id + '.png'
