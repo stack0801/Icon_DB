dcd8a693367810a860d34a57ef4429f657b3358e
MercuriKim <xsssxtntjd@naver.com>
2022-06-12 22:14:06 +0900

    Hashtaguploadtest.js 버그 수정

--- server/Hashtaguploadtest.js
+++ server/Hashtaguploadtest.js
@@ -1,7 +1,7 @@
 const sql_pool = require('./mysql')
 
-const content_id = 5
-const tag_context = '테스트2'
+const content_id = 2
+const tag_context = '테스트3'
 
 const sql_Hashtag = 'SELECT * FROM Hash WHERE Hashtag = ?'
 sql_pool.query(sql_Hashtag, [tag_context], (err_Hash, result_Hash) => {
@@ -12,18 +12,18 @@ sql_pool.query(sql_Hashtag, [tag_context], (err_Hash, result_Hash) => {
             if (err_InsertHash) {
                 console.log(err_InsertHash)
             }
-            sql_pool.query(sql_Hashtag, [tag_context], (err_Hash, rows_Hash, result_Hash) => {
-                const Hash_idx = rows_Hash[0].Hash_id
-                const sql_content_tag = 'INSERT INTO content_has_hash(content_idx, Hash_idx) VALUES (?,?)'
-                sql_pool.query(sql_content_tag, [content_id, Hash_idx], (err_content_tag, result_content_tag) => {
-                    if (err_content_tag) {
-                        console.log("중복된 값")
-                    }
-                    else {
-                        console.log(result_content_tag)
-                    }
-                })
-            })
         })
     }
+    sql_pool.query(sql_Hashtag, [tag_context], (err_Hash, rows_Hash, result_Hash) => {
+        const Hash_idx = rows_Hash[0].Hash_id
+        const sql_content_tag = 'INSERT INTO content_has_hash(content_idx, Hash_idx) VALUES (?,?)'
+        sql_pool.query(sql_content_tag, [content_id, Hash_idx], (err_content_tag, result_content_tag) => {
+            if (err_content_tag) {
+                console.log("중복된 값")
+            }
+            else {
+                console.log(result_content_tag)
+            }
+        })
+    })
 })
