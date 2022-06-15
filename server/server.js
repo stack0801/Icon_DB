ea8cb4361f2de573587ed43805d30287fbb4211d
stack0801 <stack0801@gmail.com>
2022-06-15 18:08:27 +0900

    서버사이드 정리 중

--- server/server.js
+++ server/server.js
@@ -1,36 +1,12 @@
 const express = require('express')                      //express 모듈 사용
-const app = express()
-var path = require('path')
-const sql_pool = require('./mysql')                     //mysql.js 파일 로드
-const session_stream = require('./session')
+const sql_pool = require('./src/mysql')                     //mysql.js 파일 로드
+const session_stream = require('./src/session')
 
-app.use(express.static('icons/'))                       //폴더를 클라이언트가 요청가능 (static)
-app.use(express.static('Front/'))                       //react 를 사용함으로서 추후에 삭제 가능
+const app = express()
 
 app.use(express.json())                                 //json 방식으로 통신
 app.use(express.urlencoded({ extended: false }))           //post 로 받은 값에서 req.body를 읽을 수 있게함 //근데 솔직히 뭔뜻인지 모르겠음
 app.use(session_stream)
-
-//html
-
-app.get('/', (req, res) => {                            // '/'주소로 요청받았을때
-    res.sendFile(__dirname + '/Front/html/main.html')   // __dirname + '/Front/html/main.html' 주소의 파일을 넘겨줌
-})
-
-app.get('/sign_up', (req, res) => {
-    res.sendFile(__dirname + '/Front/html/sign_up.html')
-})
-
-app.get('/sign_in', (req, res) => {
-    res.sendFile(__dirname + '/Front/html/sign_in.html')
-})
-
-app.get('/boardtest', (req, res) => {
-    if (req.session.sign)
-        res.sendFile(__dirname + '/Front/html/boardtest.html')
-    else
-        res.redirect('/')
-})
 
 //api
 
@@ -103,10 +79,6 @@ app.post('/get_contents', (req, res) => {
     })
 })
 
-app.get('/content/:id', (req, res) => {
-    res.sendFile(__dirname + '/Front/html/content.html')
-})
-
 app.post('/get_content', (req, res) => {
     const id = req.body.id
 
@@ -152,43 +124,6 @@ var upload = multer({
         limits: { fileSize: 1000 * 1000 * 10 }
     })
 })
-
-// const s3 = new AWS.S3({
-//     accessKeyID : process.env.AWS_ACCESSKEY,
-//     secretAccessKey : process.env.AWS_SECRETACCESSKEY,
-//     region : 'us-east-1'
-// });
-
-// const allowedExtensions = ['.png', '.jpg', '.svg']
-
-// const imageUploader = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'webservicegraduationproject',
-//         contentType: multerS3.AUTO_CONTENT_TYPE,
-//         key: (req, file, callback) => {
-//             callback(null, `${file.originalname}`)
-//         },
-//         acl: 'public-read-write'
-//     }),
-// })
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'icons/');
-//     },
-//     filename: (req, file, cb) => {
-//         const sql = 'select * from content order by content_id desc limit 1'
-//         sql_pool.query(sql, (err, results) => {
-//             if (err)
-//                 console.log(err);
-//             else
-//                 cb(null, "" + (results[0].content_id + 1) + path.extname(file.originalname));    //강제 jpg 형변환 추후 고려
-//         })
-//     }
-// })
-// const upload = multer({storage: storage })
-// //
 
 app.post('/boardtest', upload.single('img'), (req, res) => {
 
