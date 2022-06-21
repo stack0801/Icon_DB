497ebb9268678ad72532f1c2b1076afbea7ea341
MercuriKim <xsssxtntjd@naver.com>
2022-06-22 03:39:04 +0900

    해당 아이디가 작성한 콘텐츠 받아오기 기능 추가

--- server/test.js
+++ server/test.js
@@ -1,9 +1,8 @@
 const sql_pool = require('./src/mysql')
 
-const content_id = 'a'
-const sql_getlike = `SELECT content.* FROM likefunction INNER JOIN content ON 
-    content.content_id = likefunction.content_idx WHERE likefunction.id = ?`
-sql_pool.query(sql_getlike, [content_id], (err_get, rows_get, result_get) => {
+const id = 'a'
+const sql_getcontent = `SELECT * FROM content WHERE user_id = ?`
+sql_pool.query(sql_getcontent, [id], (err_get, rows_get, result_get) => {
     if (err_get)
         console.log(err_get)
     else
