614edbe793f8cb7026da41336226a103e5e735b0
stack0801 <stack0801@gmail.com>
2022-06-21 10:35:15 +0900

    svgedit 프로잭트 인풋

--- client/src/component/LandingPage/Top.js
+++ client/src/component/LandingPage/Top.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import SearchBox from "../SearchBox";
-import top_image from "../../img/Background.jpg";
+import top_image from "../../img/watercolor.jpg";
 
 export default function App(){
     return(
