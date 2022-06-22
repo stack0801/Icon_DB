310f61b18e0616f63247d19912d217fd4c895140
MercuriKim <xsssxtntjd@naver.com>
2022-06-22 17:37:02 +0900

    tag_insert 수정완료

--- server/server.js
+++ server/server.js
@@ -217,22 +217,16 @@ app.post('/tag_insert', (req, res) => {
     const content_id = req.body.content_id
     const tag_context = req.body.tag_context
 
-    const sql_Hashtag = 'SELECT * FROM Hash WHERE Hashtag = ?'
-    sql_pool.query(sql_Hashtag, [tag_context], (err_Hash, result_Hash) => {
-        if (result_Hash.length == 0) {
-            const sql_InsertHash = "INSERT INTO Hash(Hashtag) VALUES ('" + tag_context + "')"
-            sql_pool.query(sql_InsertHash, (err_InsertHash, result_InsertHash) => {
-                if (err_InsertHash) 
-                    console.log(err_InsertHash)
-            })
-        }
+    const sql_InsertHash = "INSERT INTO Hash(Hashtag) VALUES ('" + tag_context + "')"
+    sql_pool.query(sql_InsertHash, (err_InsertHash, result_InsertHash) => {
+        const sql_Hashtag = 'SELECT * FROM Hash WHERE Hashtag = ?'
         sql_pool.query(sql_Hashtag, [tag_context], (err_Hash, rows_Hash, result_Hash) => {
             const Hash_idx = rows_Hash[0].Hash_id
             const sql_content_tag = 'INSERT INTO content_has_hash(content_idx, Hash_idx) VALUES (?,?)'
             sql_pool.query(sql_content_tag, [content_id, Hash_idx], (err_content_tag, result_content_tag) => {
-                if (err_content_tag) 
+                if (err_content_tag)
                     res.send("duplication")
-                else 
+                else
                     res.send(result_content_tag)
             })
         })
