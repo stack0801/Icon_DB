9e7de468caa7080ca7c28657e169e865fa0333ff
bbagbbagn2 <pyoungh999@naver.com>
2024-02-01 06:05:55 +0900

    디자인 수정

--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
@@ -7,27 +8,33 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
 
-export default function App({ width, height, fontSize }) {
+export default function App() {
     const [searchbox, setSearchbox] = useState("");
     const navigate = useNavigate();
 
     const onKeyPress = (e) => { if (e.key === 'Enter') onSubmit() }
 
     const onSubmit = () => {
-        navigate(`/searching/${searchbox}`);
+        if(searchbox.trim().length >= 1){
+            navigate(`/searching/${searchbox}`);
+        } else {
+            alert("검색하시려면 최소 1자를 입력하세요");
+        }
+        
     }
 
     return (
         <Paper
             component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 500 }}
+            sx={{ p: '0 10px', display: 'flex', alignItems: 'center', width: '90%', height: '64px', background: '#eff3f6', boxShadow: 'none', borderRadius: '3px' }}
         >
             <InputBase
                 sx={{ ml: 1, flex: 1 }}
-                placeholder="아이콘을 검색해 주세요"
+                placeholder="아이콘을 검색해 보세요"
                 inputProps={{ 'aria-label': 'search your icons' }}
                 onChange={(e) => setSearchbox(e.currentTarget.value)}
                 onKeyPress={onKeyPress}
+                color="#424242"
             />
             <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={onSubmit}>
                 <SearchIcon />
