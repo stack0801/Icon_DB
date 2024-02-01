d49a47b67a9b50aafbf8f4dcafa6298de242dc13
bbagbbagn2 <pyoungh999@naver.com>
2024-02-02 06:11:02 +0900

    Logo.jsx 변경

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { ReactComponent as SvgLogo } from "@_assets/brand/logo.svg";
 
 export default function Logo() {
-  return <SvgLogo />;
-}
+    return (
+        <Link to="/">
+            <SvgLogo />
+        </Link>
+    );
+};
