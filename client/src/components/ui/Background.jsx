21573d24c672a150d70f3cf4930bc9ce85229fa2
bbagbbagn2 <pyoungh999@naver.com>
2024-08-05 22:27:43 +0900

    style: styled component 이름 변경 style 컴포넌트의 의도를 더 저확하게 하기 위해 이름을 더 구체적으로 변경

--- client/src/components/ui/Background.jsx
+++ client/src/components/ui/Background.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import background from '@_assets/images/watercolor.webp'
+import background from '@_assets/images/watercolor.webp';
 
 export default function Background() {
-    return (
-        <BackgroundBox>
-            <BackgroundImage src={background} alt='background image'></BackgroundImage>
-        </BackgroundBox>
-    );
+  return (
+    <BackgroundWrapper>
+      <BackgroundImage src={background} alt="background image" />
+    </BackgroundWrapper>
+  );
 }
 
-const BackgroundBox = styled.div`
-    position: relative;
-    height: 100%;
-    background-color: #f8fafb;
-    overflow: hidden;
+const BackgroundWrapper = styled.div`
+  position: relative;
+  height: 100%;
+  background-color: #f8fafb;
+  overflow: hidden;
 
-    @media only screen and (min-width: 992px) {
-        width: calc(100% - 480px);
-    }
+  @media only screen and (min-width: 992px) {
+    width: calc(100% - 480px);
+  }
 `;
 
 const BackgroundImage = styled.img`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 100%;
-    height: 100%;
-    transform: translate(-50%, -50%);
-    filter: brightness(75%);
-    object-fit: cover;
-    pointer-events: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  transform: translate(-50%, -50%);
+  filter: brightness(75%);
+  object-fit: cover;
+  pointer-events: none;
 `;
