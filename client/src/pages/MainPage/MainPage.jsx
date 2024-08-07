e6ebf92617e9bc806853d55a8ba2c01b390d28e8
bbagbbagn2 <pyoungh999@naver.com>
2024-08-08 02:05:04 +0900

    style: main 태그 styled-components 생성 main 태그 스타일 변경

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
-import Banner from "./components/Banner";
-import Popular from "./components/PopularSection";
-import LogoSection from "./components/LogoSection";
-import TopIconSearch from "./components/TopIconSearch";
-import Header from "@_components/common/Header/Header";
-import TopButton from "@_components/ToTopButton";
-import Footer from "@_components/common/Footer/Footer";
+import Banner from './components/Banner';
+import Popular from './components/PopularSection';
+import LogoSection from './components/LogoSection';
+import TopIconSearch from './components/TopIconSearch';
+import Header from '@_components/common/Header/Header';
+import TopButton from '@_components/ToTopButton';
+import Footer from '@_components/common/Footer/Footer';
 
 export default function MainPage() {
   useEffect(() => {
-    document.body.classList.add("hero--white");
+    document.body.classList.add('hero--white');
 
     return () => {
-      document.body.classList.remove("hero--white");
+      document.body.classList.remove('hero--white');
     };
   }, []);
 
@@ -21,13 +22,17 @@ export default function MainPage() {
     <>
       <Header />
       <TopButton />
-      <main className="home-icons">
+      <StyledMain className="home-icons">
         <Banner />
         <Popular />
         <LogoSection />
         <TopIconSearch />
-      </main>
+      </StyledMain>
       <Footer />
     </>
   );
 }
+
+const StyledMain = styled.main`
+  padding-top: 118px;
+`;
